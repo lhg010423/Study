e6495c5c92be48e9e5b6b34e3a57f66179dfca14
lhg010423 <lhg010423@gmail.com>
2026-01-23 00:18:27 +0900

    20260123_0018

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // alt + insert -> constructor 선택
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
