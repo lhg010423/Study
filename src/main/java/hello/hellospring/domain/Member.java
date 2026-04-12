da60b85d31a573245aacc5fc34395727aadc51ce
lhg010423 <lhg010423@gmail.com>
2026-04-12 21:56:35 +0900

    20260412

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,16 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 시퀀스처럼 시스템이 지정하는 값으로 함
+
     private String name;
 
     public Long getId() {
